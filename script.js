d895ab36719bffffd10d25fbffd110c20e49e0e3
Rishav Jain <jainrishav632@gmail.com>
2025-08-21 01:18:16 +0530

    Add Task Fixes

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ function addTask(){
     let theTask = document.createElement(`div`);
     theTask.innerText = task;
     myTask.prepend(myCheckBox, theTask);
+    myTask.classList.add(`myTask`);
+    myCheckBox.classList.add(`myCheckbox`);
+    theTask.classList.add(`theTask`);
 
     let containsControls=  document.createElement(`div`);
     let containsDueTime=  document.createElement(`div`);
@@ -47,13 +50,24 @@ function addTask(){
     let binIcon = document.createElement(`div`);
     binIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`;
 
-    
     otherBtns.append(subTaskIcon,ScheduleTime,binIcon);
+    otherBtns.classList.add(`otherBtns`);
     containsDueTime.append(dueTimeIcon,time);
+    containsDueTime.classList.add(`containsDueTime`);
     containsControls.append(containsDueTime,otherBtns);
+    containsControls.classList.add(`containsControls`);
     ContainsTaskAndControls.append(myTask,containsControls);
+    ContainsTaskAndControls.classList.add(`ContainsTaskAndControls`);
     BigTaskBox.append(ContainsTaskAndControls,containsSubTask)
+    BigTaskBox.classList.add(`BigTaskBox`);
     body.prepend(BigTaskBox);
+
+    BigTaskBox.addEventListener(`mouseover`,()=>{
+        containsControls.style.display = `block`;
+    })
+    BigTaskBox.addEventListener(`mouseout`,()=>{
+        containsControls.style.display = `none`;
+    })
 
     document.querySelector(`#input`).value = "";
     document.querySelector(`#input`).style.height = 'auto';
